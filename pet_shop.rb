c35a935bebc9ae01406873157ba9e762a9f40caf
Ross Hill <rosshill88@gmail.com>
2017-11-24 14:16:09 +0000

    Initial commit

---
+++ pet_shop.rb
@@ -0,0 +1,15 @@
+def pet_shop_name(pet_shop)
+  return pet_shop[:name]
+end
+
+def total_cash(pet_shop)
+  return pet_shop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(pet_shop, cash)
+  pet_shop[:admin][:total_cash] += cash
+end
+
+def pets_sold(pet_shop)
+  pet_shop[:admin][:pets_sold]
+end
