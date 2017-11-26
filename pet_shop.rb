bc2a1143907ffdb3a117608ab582581157d0b733
Ross Hill <rosshill88@gmail.com>
2017-11-26 18:25:21 +0000

    First 2 optional tests running

--- pet_shop.rb
+++ pet_shop.rb
@@ -64,3 +64,11 @@ def add_pet_to_customer(customers, new_pet)
   customers[:pets].push(new_pet)
   customers[:pets].length
 end
+
+def customer_can_afford_pet(customer,new_pet)
+  if customer[:cash] > new_pet[:price]
+      return true
+  else
+      return false
+  end
+end
